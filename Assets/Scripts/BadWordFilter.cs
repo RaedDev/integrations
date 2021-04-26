12a86f3c1f213d097d2a750396b34ab7b65483ba
Raed Abdallah <raed.dev@gmail.com>
2021-04-26 20:40:50 +0300

    bad word filter

---
+++ Assets/Scripts/BadWordFilter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public static class BadWordFilter {
+
+    static string[] patterns = { 
+        "\\b[a@]ss\\b",
+        "fuck",
+        "bitch",
+        "\\bكس\\b"
+    };
+
+    public static string Filter(string str)
+    {
+        return Regex.Replace(str, string.Join("|", patterns), a => new string('*', a.Length));
+    }
+}
